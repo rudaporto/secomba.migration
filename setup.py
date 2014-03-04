56a809433d763d629acfefbdcfa2a3e6d5eb0dce
root <root@saobento4.ba.gov.br>
2014-03-04 17:15:01 -0300

    Update package dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
 from setuptools import setup, find_packages
-import os
+import os,sys
 
 version = '1.0'
+
+if sys.version_info < (2, 5):
+	    # Python 2.4 seems to have issues with setuptools collecting its
+    # requirement packages from PyPI using HTTPS. This has been encountered
+    # using Python 2.4.3/x86 on Windows 7/SP1/x64 with setuptools 0.7.2. As a
+    # workaround we replace setuptools's PackageIndex class with one that
+    # always uses the HTTP transfer protocol instead of HTTPS when dealing with
+    # this Python version.
+    #
+    # Note that this workaround affects only setuptools's automated requirement
+    # package downloading. Any requirement packages can still be installed
+    # manually by the user, using a suitable package index source URL.
+    import setuptools.package_index
+    OriginalPackageIndex = setuptools.package_index.PackageIndex
+    class NoHTTPSPackageIndex(OriginalPackageIndex):
+        def __init__(self, *args, **kwargs):
+            OriginalPackageIndex.__init__(self, *args, **kwargs)
+            cue = "https:"
+            if self.index_url.lower().startswith(cue):
+	        self.index_url = "http:" + self.index_url[len(cue):]
+    setuptools.package_index.PackageIndex = NoHTTPSPackageIndex
 
 setup(name='secomba.migration',
       version=version,
@@ -17,7 +38,7 @@ setup(name='secomba.migration',
       keywords='',
       author='',
       author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      url='https://github.com/rudaporto/secomba.migration.git',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['secomba', 'secomba.migration'],
@@ -25,6 +46,9 @@ setup(name='secomba.migration',
       zip_safe=False,
       install_requires=[
           'setuptools',
+	  'MySQL-python',
+	  'zope.sqlalchemy==0.6.1',
+	  'sqlalchemy<0.7-dev',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
