1182ff103f2b6da91e1827eab59a49b038da399b
Ruda Porto Filgueiras <rudazz@gmail.com>
2014-03-05 03:30:23 -0300

    New views, one for each content to migrate: Audio, Photo, Album, Video, Relation.

--- secomba/migration/views.py
+++ secomba/migration/views.py
@@ -1,9 +1,34 @@
 from Products.Five import BrowserView
 
-from run import migrate_noticia
+from secomba.migration import run
 
 class NoticiaMigrationView(BrowserView):
 
     def __call__(self):
-        return migrate_noticia(self.context)
+        return run.migrate_noticia(self.context)
+
+class AlbumMigrationView(BrowserView):
+
+    def __call__(self):
+        return run.migrate_album(self.context)
+
+class PhotoMigrationView(BrowserView):
+
+    def __call__(self):
+        return run.migrate_photo(self.context)
+
+class AudioMigrationView(BrowserView):
+
+    def __call__(self):
+        return run.migrate_audio(self.context)
+
+class VideoMigrationView(BrowserView):
+
+    def __call__(self):
+        return run.migrate_video(self.context)
+
+class RelationMigrationView(BrowserView):
+
+    def __call__(self):
+        return run.migrate_relation(self.context)
 
